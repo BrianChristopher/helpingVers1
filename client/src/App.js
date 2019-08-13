5e48aceb4e96f28c6f3a3a4ccb9d5894a886db20
Brian Christopher <bcwood79@yahoo.com>
2019-08-13 13:09:10 -0400

    Basic NavBar and component page landings coded.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./components/home";
+//import NotFound from "./components/notFound";
+import NavBar from "./components/common/navBar";
+import Login from './components/login';
+import Register from './components/register';
 import "./App.css";
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            {/* <Route path="/not-found" component={NotFound} /> */}
+            <Redirect from="/" exact to="/home" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ client/src/components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <h1>Home Page Here</h1>
+      );
+}
+ 
+export default Home;
