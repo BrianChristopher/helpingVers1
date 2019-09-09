40e5bba9f070f0ca0273ec4165493857eca05557
Brian Christopher <bcwood79@yahoo.com>
2019-09-08 20:51:55 -0400

    Meal items pill components changed from badges to buttons.

--- client/src/components/common/mealPill.jsx
+++ client/src/components/common/mealPill.jsx
@@ -3,19 +3,53 @@ import Badge from "react-bootstrap/Badge";
 
 class MealPill extends Component {
   render() {
-      const {name, location, id} = this.props.item;
+    const { name, location, id } = this.props.item;
     return (
-      <h6
+      <div
+        class="btn-group menuItemButton"
         value={id}
         name={name}
         location={location}
         draggable
         onDragStart={e => this.props.handleDragStart(e, this.props.item._id)}
       >
-        <Badge className="menuItemPill" pill variant="primary">
+        <button type="button" class="btn btn-sm btn-block btn-primary">
           {name}
-        </Badge>
-      </h6>
+        </button>
+        <button
+          type="button"
+          class="btn btn-sm btn-primary dropdown-toggle dropdown-toggle-split"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Toggle Dropdown</span>
+        </button>
+
+        <div class="dropdown-menu">
+          <button class="dropdown-item" type="button">
+            Info
+          </button>
+          <button class="dropdown-item" type="button">
+            Edit
+          </button>
+          <button class="dropdown-item" type="button">
+            Delete
+          </button>
+        </div>
+      </div>
+
+      // <h6
+      //   value={id}
+      //   name={name}
+      //   location={location}
+      //   draggable
+      //   onDragStart={e => this.props.handleDragStart(e, this.props.item._id)}
+      // >
+      //   <Badge className="menuItemPill" pill variant="primary">
+      //     {name}
+      //   </Badge>
+      // </h6>
     );
   }
 }
